0726f3f711d0d20586e7dbcb1aa1c1e970837eca
surajalgox360 <suraj@algox360.com>
2024-11-13 19:25:04 +0530

    server page

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename)
 import { createServer } from 'http';
 import DBConnection from './config/DBconnect.js';
 import routes from './routes/index.js';
 import morgan from 'morgan';
+import path from 'path';
+
+
 
 const app = express();
 const PORT = process.env.PORT || 8810;
 
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
-app.use(morgan('combined'));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+
 
 app.get('/', function(req,res){
-    res.end('MUNIM SERVER LISTENING');
+    res.render('index')
 })
 
 app.use('/api',routes);
