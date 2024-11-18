b475222fbbbff15928f35c34e92facafa661a781
surajalgox360 <suraj@algox360.com>
2024-11-18 10:57:45 +0530

    certificate

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename)
-import { createServer } from 'http';
+import { createServer } from 'https';
 import DBConnection from './config/DBconnect.js';
 import routes from './routes/index.js';
 import path from 'path';
@@ -20,14 +20,24 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 
-
 app.get('/', function(req,res){
     res.render('index')
 })
 
 app.use('/api',routes);
 
-createServer(app).listen(PORT, ()=> console.log(`MUNIM's Server Listening (${PORT})`));
+// Load SSL certificates
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/kambojproperty.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/kambojproperty.com/fullchain.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/kambojproperty.com/chain.pem', 'utf8'); 
+
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca
+};
+
+createServer(credentials, app).listen(PORT, ()=> console.log(`MUNIM's Server Listening (${PORT})`));
 
 
 
