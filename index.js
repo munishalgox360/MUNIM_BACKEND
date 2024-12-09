bf867c97d68742601bfa000bcf0d3769af8557ee
surajalgox360 <suraj@algox360.com>
2024-12-09 13:37:48 +0530

    get user profile-

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename)
-import { createServer } from 'http';
+import { createServer } from 'https';
 import DBConnection from './config/DBconnect.js';
 import routes from './routes/index.js';
 import path from 'path';
@@ -30,18 +30,18 @@ app.get('/', function(req,res){
 app.use('/api',routes);
 
 // Load SSL certificates
-// const privateKey = fs.readFileSync('/etc/letsencrypt/live/kambojproperty.com/privkey.pem', 'utf8');
-// const certificate = fs.readFileSync('/etc/letsencrypt/live/kambojproperty.com/fullchain.pem', 'utf8');
-// const ca = fs.readFileSync('/etc/letsencrypt/live/kambojproperty.com/chain.pem', 'utf8'); 
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/kambojproperty.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/kambojproperty.com/fullchain.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/kambojproperty.com/chain.pem', 'utf8'); 
 
-// const credentials = {
-//   key: privateKey,
-//   cert: certificate,
-//   ca: ca
-// };
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca
+};
 
-// createServer(credentials, app).listen(PORT, ()=> console.log(`MUNIM's Server Listening (${PORT})`));
-createServer(app).listen(PORT, ()=> console.log(`MUNIM's Server Listening (${PORT})`));
+createServer(credentials, app).listen(PORT, ()=> console.log(`MUNIM's Server Listening (${PORT})`));
+// createServer(app).listen(PORT, ()=> console.log(`MUNIM's Server Listening (${PORT})`));
 
 
 
